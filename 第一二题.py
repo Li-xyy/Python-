b7195e94660d5ca1a4bec27d4e9d6a433232c2c0
Li-xyy <40747850+Li-xyy@users.noreply.github.com>
2018-07-02 11:07:31 +0800

    Update 第一二题.py

--- 第一二题.py
+++ 第一二题.py
@@ -19,15 +19,3 @@ msg={'温度':['22','21','20','22','23','25','22'],'天气':['晴','雨','雨','
 msg['温度'][2]
 msg['天气'][2]
 msg['最高温度']
-第三题
-import urllib.request as r
-url='http://api.openweathermap.org/data/2.5/weather?q=chongqing&mode=json&units=metric&lang=zh_cn&APPID=6a67ed641c0fda8b69715c43518b6996'
-data=r.urlopen(url).read().decode('utf-8')
-import json
-data=json.loads(data)
-data['main']['temp']
-data['weather'][0]['description']
-data['main']['pressure']
-print('重庆温度是：'+str(data['main']['temp']))
-print('重庆天气情况是：'+str(data['weather'][0]['description']))
-print('重庆天气气压是：'+str(data['main']['temp']))
